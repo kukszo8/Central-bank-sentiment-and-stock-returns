683a470ae637a216bbe28b54b257e624f6c3887f
MarcellGranat <granatcellmar98@gmail.com>
2025-04-28 01:46:49 +0200

    Refactor speech downloading logic and improve file writing structure

--- workflow/scrape_fed.py
+++ workflow/scrape_fed.py
@@ -7,23 +7,23 @@ from tqdm import tqdm
 
 json_path = "https://www.federalreserve.gov/json/ne-speeches.json"
 
-speeches_meta: list = requests.get(json_path).content.decode("utf-8-sig")
+speeches_meta: str = requests.get(json_path).content.decode("utf-8-sig")
 
-speeches_meta_list = json.loads(speeches_meta)
+metas: list = json.loads(speeches_meta)
 
-with open("data/fed_speeches_meta.jsonl", "w") as file:
-    for meta in speeches_meta_list:
-        file.write(json.dumps(meta) + "\n")
-
-
-with open("data/fed_speeches_body.jsonl", "w") as file:
-    for meta in tqdm(speeches_meta_list[0:7], desc="Downloading speeches", colour="green"):
+for meta in tqdm(metas, desc="Downloading speeches", colour="green"):
+    try:
         response = requests.get("https://www.federalreserve.gov" + meta.get("l"))
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, "html.parser")
             paragraphs = [p.get_text() for p in soup.find_all("p")]
             content = "\n\n".join(paragraphs)
-            file.write(json.dumps({"content": content}) + "\n")
+            # * append to meta only if the content is available
+            with open("data/fed_speeches_meta.jsonl", "w") as file:
+                file.write(json.dumps(meta) + "\n")
+            with open("data/fed_speeches_body.jsonl", "a") as file:
+                file.write(json.dumps({"content": content}) + "\n")
         else:
             print(f"Failed to download {meta.get('l')}: {response.status_code}")
-            file.write(json.dumps({"content": None}) + "\n")
+    except Exception as e:
+        print(f"Error processing {meta.get('l')}: {e}")
